b3e12ff0a2bdb25f1fafaf50b1b960bee62e3a37
eduardomarcialhdez <edumarher15@outlook.com>
2023-02-27 11:16:48 -0600

    Bloqueo del menú emergente con el clic derecho

--- resources/views/plantilla_visit.blade.php
+++ resources/views/plantilla_visit.blade.php
@@ -4,6 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bloqueo del clic derecho -->
+    <script>
+        window.onload = function() {
+            document.addEventListener("contextmenu", function(e) {
+                e.preventDefault();
+            });
+        }
+    </script>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" href="{{ asset('img/logo.png') }}">
