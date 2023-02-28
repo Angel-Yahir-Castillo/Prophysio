435559bba35e1433a613938c5a8005242bb624f4
Eduardo Marcial Hernández <111074954+eduardomarcialhdez@users.noreply.github.com>
2023-02-28 09:14:37 -0600

    PROP-54-Realizar-copias-de-seguridad-cada-cierto-periodo-de-tiempo (#3)
    * poniendo funcion
    
    * Creación de copias de seguridad
    
    ---------
    
    Co-authored-by: Angel Yahir <yahirgamerpvz@gmail.com>

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,20 +4,49 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\user;
+use ZipArchive;
+
 class AdminController extends Controller
 {
-    public function index(){
+
+    public function index()
+    {
         //$usuarios = User::all();
         //return $usuarios;
         $respuesta =  ' ';
         return view('admin.admin_login', compact('respuesta'));
     }
 
-    public function inicio(){
+    public function inicio()
+    {
         return view('admin.home');
     }
 
+    public function respaldar()
+    {
+        $nombre = "prophysio";
+        $usuario = "root";
+        $password = "";
 
+        $fecha = date('Y-m-d_His');
 
+        $nombre_sql = $nombre . '-' . $fecha . '.sql';
 
+        $dump = "mysqldump --user=$usuario --password=$password $nombre > $nombre_sql";
+
+        exec($dump);
+
+        //para guardar en .zip
+        $zip = new ZipArchive();
+
+        $nombre_zip = $nombre . '-' . $fecha . '.zip';
+
+        if ($zip->open($nombre_zip, ZipArchive::CREATE) === true) {
+            $zip->addFile($nombre_sql);
+            $zip->close();
+            unlink($nombre_sql);
+            header("Location: $nombre_zip");
+            exit();
+        };
+    }
 }
