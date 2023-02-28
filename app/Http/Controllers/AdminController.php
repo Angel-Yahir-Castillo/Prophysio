18c07ffc916f9b3e928881c8c270789dda2acb25
eduardomarcialhdez <edumarher15@outlook.com>
2023-02-28 09:07:17 -0600

    Creación de copias de seguridad

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,48 +9,44 @@ use ZipArchive;
 class AdminController extends Controller
 {
 
-    public function index(){
+    public function index()
+    {
         //$usuarios = User::all();
         //return $usuarios;
         $respuesta =  ' ';
         return view('admin.admin_login', compact('respuesta'));
     }
 
-    public function inicio(){
+    public function inicio()
+    {
         return view('admin.home');
     }
 
-
-
-    public function respaldar(){
-
-        $nombre = "";
+    public function respaldar()
+    {
+        $nombre = "prophysio";
         $usuario = "root";
         $password = "";
-    
+
         $fecha = date('Y-m-d_His');
-    
-        $nombre_sql = $nombre.'-'.$fecha.'.sql';
-    
+
+        $nombre_sql = $nombre . '-' . $fecha . '.sql';
+
         $dump = "mysqldump --user=$usuario --password=$password $nombre > $nombre_sql";
-    
+
         exec($dump);
-    
+
         //para guardar en .zip
         $zip = new ZipArchive();
-    
-        $nombre_zip = $nombre.'-'.$fecha.'.zip';
-    
-        if($zip->open($nombre_zip, ZipArchive::CREATE) === true){
+
+        $nombre_zip = $nombre . '-' . $fecha . '.zip';
+
+        if ($zip->open($nombre_zip, ZipArchive::CREATE) === true) {
             $zip->addFile($nombre_sql);
             $zip->close();
             unlink($nombre_sql);
             header("Location: $nombre_zip");
             exit();
-            //return redirect(route('home'));
         };
-
-        
     }
-
 }
