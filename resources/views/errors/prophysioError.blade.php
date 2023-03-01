3dbcdb4b4956c75264335bd900f38ac872fe4c97
Monsserrath <20200790@uthh.edu.mx>
2023-03-01 16:00:09 -0600

    mensaje de error

--- resources/views/errors/prophysioError.blade.php
+++ resources/views/errors/prophysioError.blade.php
@@ -38,8 +38,6 @@
                 </div>
             </div>
         </div>
-
-
          <!-- Compiled and minified JavaScript -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
